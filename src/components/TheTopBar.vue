3bbfd6d8e017c5f2bc68bb379b303e2d3a200e32
Łukasz Samcik <l.samcik@gmail.com>
2024-01-22 15:18:30 +0100

    added styles to TheTopBar

--- src/components/TheTopBar.vue
+++ src/components/TheTopBar.vue
@@ -16,6 +16,7 @@ const isHamburgerActive = ref(false)
       <div class="relative w-full bg-white px-[1rem] py-[0.8rem] lg:px-[4rem] md:px-[2rem]">
         <div v-show="isHamburgerActive" class="item-shadow absolute right-0 top-14 max-w-[12rem] w-full bg-white px-[1rem] py-[2rem] md:hidden">
           <nav>
+            <img src="/logo.png" alt="">
             <ul class="flex flex-col gap-1 lg:text-[1rem] sm:text-[0.8rem]">
               <li v-for="(item, index) in navItems" :key="index" class="flex rounded-[8rem] py-[0.2rem] pl-[1rem] font-700 transition-colors duration-500 hover:bg-purple-100">
                 <a class="w-full" @click="isHamburgerActive = false">
@@ -29,18 +30,18 @@ const isHamburgerActive = ref(false)
           <a href="/" class="flex items-center gap-[0.2rem] font-extrabold sm:gap-[0.5rem]">
             <img class="" src="/logo.png" alt="logo">
           </a>
-          <nav class="flex">
-            <ul class="hidden gap-[0.8rem] lg:flex lg:gap-[1rem] lg:text-[1rem] sm:text-[0.8rem]">
-              <li v-for="(item, index) in navItems" :key="index" class="rounded-[8rem] px-[0.5rem] py-[0.2rem] font-700 transition-colors duration-1000 hover:bg-purple-200 lg:px-[0.8rem]">
+          <nav class="flex items-center">
+            <ul class="hidden gap-[0.8rem] lg:flex lg:gap-[2.190625rem] lg:text-[1rem] sm:text-[0.8rem]">
+              <li v-for="(item, index) in navItems" :key="index" class="rounded-[8rem] font-700">
                 <a>
                   {{ item.name }}
                 </a>
               </li>
             </ul>
-            <button class="hidden bg-[#F89E52] md:flex">
+            <button class="hidden rounded-[1.25rem] bg-[#F89E52] px-[2.0625rem] py-[1.1875rem] text-white md:flex">
               Speak With Us!
             </button>
-            <div class="flex items-center gap-2 sm:gap-10">
+            <div class="flex items-center items-center gap-2 sm:gap-10">
               <button class="lg:hidden" type="button" @click="isHamburgerActive = !isHamburgerActive">
                 <img v-if="!isHamburgerActive" src="/hamburger.png" alt="">
                 <img v-if="isHamburgerActive" src="/exit.png" alt="">
@@ -58,12 +59,6 @@ const isHamburgerActive = ref(false)
 
 <style scoped>
 .item-shadow {
-  box-shadow:
-    0px 4px 4px -2px rgba(0, 0, 0, 0.06),
-    0px 4px 8px -2px rgba(0, 0, 0, 0.1);
-}
-
-.wrapper {
-  padding-top: v-bind('topBarPaddingTop');
+  box-shadow: 0px 10px 15px 0px rgba(25, 25, 25, 0.1);
 }
 </style>
