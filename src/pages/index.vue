39b91bfa88fe88cb73c079435b495b1c8b9258b5
Łukasz Samcik <l.samcik@gmail.com>
2024-01-23 10:41:47 +0100

    added new imgs for first section for tablet

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@ const icons = ['/linkedinIcon.png', '/facebookIcon.png', '/instagramIcon.png', '
 <template>
   <div>
     <section class="mx-[0.9375rem] mt-[2.211875rem] flex flex-col">
-      <button type="button" class="mx-auto rounded-[2rem] bg-[#F89E52] px-[1.3125rem] py-[1.4375rem] text-[1.125rem] text-[#FFF1E7] font-500 sm:py-[0.75rem]">
+      <button type="button" class="mx-auto rounded-[2rem] bg-[#F89E52] px-[1.3125rem] py-[1.4375rem] text-[0.8rem] text-[#FFF1E7] font-500 sm:py-[0.75rem] sm:text-[1.125rem]">
         Social Media Marketing That Works
       </button>
       <h1 class="mt-1.25rem text-center text-[2.2rem] text-[#2A2A2A] font-700 sm:text-[4.5rem]">
@@ -13,10 +13,12 @@ const icons = ['/linkedinIcon.png', '/facebookIcon.png', '/instagramIcon.png', '
       </h1>
       <div class="mt-[2rem] flex justify-center gap-[0.4875rem]">
         <div class="flex flex-col gap-[0.4875rem]">
-          <img src="/bakeryTop.png" alt="">
-          <img src="/bakeryBottom.png" alt="">
+          <img class="" src="/bakeryTopTablet.png" alt="">
+          <img class="" src="/bakeryBottomTablet.png" alt="">
         </div>
-        <img src="/beachRight.png" alt="">
+        <div>
+          <img class="" src="/beachRightTablet.png" alt="">
+        </div>
       </div>
       <div class="mt-[2rem] flex justify-center gap-[1.25rem]">
         <button v-for="(icon, index) in icons" :key="index">
