5e6223dc9964c770fcc015311b5dfdad54fd367d
Łukasz Samcik <l.samcik@gmail.com>
2024-01-23 18:31:08 +0100

    added counter for scroll

--- src/components/AppCounter.vue
+++ src/components/AppCounter.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Vue3autocounter from 'vue3-autocounter'
+import { vIntersectionObserver } from '@vueuse/components'
 
 defineProps<{
   start: number
@@ -8,10 +9,23 @@ defineProps<{
   duration?: number
   color: string
 }>()
+const isInitialized = ref(false)
+const counter = ref()
+
+function startCounting() {
+  counter.value.start()
+}
+
+function onIntersectionObserver([{ isIntersecting }]: IntersectionObserverEntry[]) {
+  if (!isIntersecting || isInitialized.value)
+    return
+  startCounting()
+  isInitialized.value = true
+}
 </script>
 
 <template>
-  <div class="">
-    <Vue3autocounter :class="color" :start-amount="start" :end-amount="end" :duration="duration" :suffix="suffix" />
+  <div v-intersection-observer="onIntersectionObserver">
+    <Vue3autocounter ref="counter" class="text-[2.75rem] font-700" :class="color" :start-amount="start" :end-amount="end" :duration="duration" :autoinit="false" :suffix="suffix" />
   </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -47,27 +47,65 @@ const icons = ['/linkedinIcon.png', '/facebookIcon.png', '/instagramIcon.png', '
         </div>
       </div>
     </section>
-    <section class="relative mt-[10rem] h-[100rem] w-full bg-[#292F3E] pt-[10rem]">
+    <section class="relative mt-[10rem] w-full bg-[#292F3E] pb-[4rem] pt-[10rem]">
       <div class="absolute mx-[1rem] mt-[5.625rem] flex items-center rounded-[1.25rem] bg-#1D1D25 -top-[11rem]">
         <AppMarquee />
       </div>
-      <div class="flex flex-col gap-[1.530625rem]">
-        <div class="mx-auto max-w-[13.75rem] w-full flex flex-col items-center justify-center gap-[0.75rem] rounded-[1.25rem] bg-white py-[4.273125rem]">
-          <AppCounter :start="0" :end="225" :duration="8" color="text-orange" />
-          <span>Offices</span>
+      <div class="mx-auto max-w-[690px] flex flex-col justify-center gap-[1.530625rem] md:flex-row">
+        <div class="mx-auto mt-[5rem] max-w-[15.625rem] w-full flex flex-col gap-[1.530625rem]">
+          <div class="mx-auto max-w-[15.625rem] w-full flex flex-col items-center justify-center gap-[0.75rem] rounded-[1.25rem] bg-white py-[4.273125rem] md:mx-0">
+            <AppCounter :start="0" :end="225" :duration="2" color="text-orange" />
+            <span>Offices</span>
+          </div>
+          <div class="mx-auto max-w-[15.625rem] w-full flex flex-col items-center justify-center gap-[0.75rem] rounded-[1.25rem] bg-white py-[4.273125rem] md:mx-0">
+            <AppCounter :start="0" :end="10" :duration="2" color="text-#265FDB" />
+            <span>Workers</span>
+          </div>
         </div>
-        <div class="mx-auto max-w-[13.75rem] w-full flex flex-col items-center justify-center gap-[0.75rem] rounded-[1.25rem] bg-white py-[4.273125rem]">
-          <AppCounter :start="0" :end="10" :duration="2" color="text-#265FDB" />
-          <span>Workers</span>
+        <div class="mx-auto max-w-[15.625rem] w-full flex flex-col gap-[1.530625rem]">
+          <div class="mx-auto max-w-[15.625rem] w-full flex flex-col items-center justify-center gap-[0.75rem] rounded-[1.25rem] bg-white py-[4.273125rem] md:mx-0">
+            <AppCounter :start="0" :end="15" suffix="K" :duration="2" color="text-#57C0CE" />
+            <span>Customers</span>
+          </div>
+          <div class="mx-auto max-w-[15.625rem] w-full flex flex-col items-center justify-center gap-[0.75rem] rounded-[1.25rem] bg-white py-[4.273125rem] md:mx-0">
+            <AppCounter :start="0" :end="1300" suffix="+" :duration="2" color="text-purple" />
+            <span>Projects</span>
+          </div>
         </div>
-        <div class="mx-auto max-w-[13.75rem] w-full flex flex-col items-center justify-center gap-[0.75rem] rounded-[1.25rem] bg-white py-[4.273125rem]">
-          <AppCounter :start="0" :end="15" suffix="K" :duration="1" color="text-#57C0CE" />
-          <span>Customers</span>
+      </div>
+      <div class="mx-auto max-w-[22.5rem] flex flex-col items-center">
+        <span class="mt-[4.8444375rem] text-[1.25rem] text-[#FFD437] font-500">Social Media Management is</span>
+        <div class="mt-[1.25rem] flex flex-col text-center text-[2.85625rem] text-white font-700">
+          <h2>
+            Building
+            Brands
+          </h2>
+          <h2>
+            Creating
+            Connections
+          </h2>
+          <h2>Driving Sales</h2>
         </div>
-        <div class="mx-auto max-w-[13.75rem] w-full flex flex-col items-center justify-center gap-[0.75rem] rounded-[1.25rem] bg-white py-[4.273125rem]">
-          <AppCounter :start="0" :end="1300" suffix="+" :duration="4" color="text-purple" />
-          <span>Projects</span>
+        <div>
+          <p class="mt-[1.6875rem] text-center text-[1.2rem] text-[#BBB] font-300">
+            Social Media is more than just posting and showing off how many
+            followers you have. It's a valuable
+            addition to your marketing strategy that builds trust and creates connections with potential customers to drive sales and increase your revenue.
+          </p>
+          <p class="mt-[1.6875rem] text-center text-[1.2rem] text-[#BBB] font-300">
+            Save your time and money by
+            partnering with us and start seeing results that give you the highest return on investment
+          </p>
+          <p class="mt-[1.6875rem] text-center text-[1.2rem] text-[#BBB] font-300">
+            After all; when you grow, we grow together.
+          </p>
         </div>
+        <button type="button" class="mx-auto mt-[0.98875rem] w-full rounded-[1.25rem] bg-[#3482FF] py-[1.4375rem] text-white lg:mx-0 sm:max-w-[14.4375rem]">
+          Contact Us
+        </button>
+        <button type="button" class="mt-[1.5531rem] text-[#3486FA]">
+          How We Can Help
+        </button>
       </div>
     </section>
   </div>
