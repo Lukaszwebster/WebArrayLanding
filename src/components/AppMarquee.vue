4bfab61fe270378c7a8afdbbc4872ba48234f36a
Łukasz Samcik <l.samcik@gmail.com>
2024-02-15 15:49:22 +0100

    better styles for AppMarquee and TheForm

--- src/components/AppMarquee.vue
+++ src/components/AppMarquee.vue
@@ -6,7 +6,7 @@ import { Vue3Marquee } from 'vue3-marquee'
   <div>
     <Vue3Marquee>
       <div class="flex items-center gap-[2rem] py-[4rem] lg:gap-[4rem]">
-        <img class="ml-[2rem]" src="/entrepreneur-logo.png">
+        <img src="/entrepreneur-logo.png">
         <img src="/google-logo.png">
         <img src="/facebook-logo.png">
         <img src="/meta-logo.png">
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -12,8 +12,10 @@ const isActive = ref()
       <input class="border border-[#D5D5D5] rounded-[0.8125rem] bg-[#FBFDFF] px-[1.8125rem] py-[1.40625rem] placeholder-[#0B0B0B]" type="text" placeholder="Name">
       <input class="border border-[#D5D5D5] rounded-[0.8125rem] bg-[#FBFDFF] px-[1.8125rem] py-[1.40625rem] placeholder-[#0B0B0B]" type="text" placeholder="Email Address">
       <input class="border border-[#D5D5D5] rounded-[0.8125rem] bg-[#FBFDFF] px-[1.8125rem] py-[1.40625rem] placeholder-[#0B0B0B]" type="text" placeholder="Phone Number">
-      <select class="stylized-select border border-[#D5D5D5] rounded-[0.8125rem] bg-[#FBFDFF] px-[1.8125rem] py-[1.40625rem] placeholder-[#0B0B0B]">
-        <option>Subject</option>
+      <select class="border border-[#D5D5D5] rounded-[0.8125rem] bg-[#FBFDFF] px-[1.8125rem] py-[1.40625rem] placeholder-[#0B0B0B]">
+        <option>
+          Subject
+        </option>
         <option>Subject</option>
       </select>
       <button type="button" class="max-w-[22.5rem] w-full flex justify-center rounded-[1.25rem] bg-[#3482FF] px-[3.4375rem] py-[1.43rem] text-white md:max-w-none" @click="isActive = !isActive">
